d5486b4ff7dbb15173328d5de45b698b6731f3e8
Emily Burnham <60051506+Aderyn1121@users.noreply.github.com>
2020-07-05 19:02:07 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,13 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-class Instrument(db.Model):
-    __tablename__ = 'instruments'
-    id = db.Column(db.Integer, primary_key=True)
-    date_bought = db.Column(db.Date, nullable=False)
-    nickname = db.Column(db.String(50), nullable=False)
-    year = db.Column(db.Integer)
-    maker = db.Column(db.String(50))
-    type = db.Column(db.String(50), nullable=False)
-    used = db.Column(db.Boolean, nullable=False)
+# Left as example
+# class Instrument(db.Model):
+#     __tablename__ = 'instruments'
+#     id = db.Column(db.Integer, primary_key=True)
+#     date_bought = db.Column(db.Date, nullable=False)
+#     nickname = db.Column(db.String(50), nullable=False)
+#     year = db.Column(db.Integer)
+#     maker = db.Column(db.String(50))
+#     type = db.Column(db.String(50), nullable=False)
+#     used = db.Column(db.Boolean, nullable=False)
