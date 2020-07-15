0eebcbb4c3768d90078d0b99f9ff055c8660858e
aderyn1121 <zaristus@gmail.com>
2020-07-15 10:00:57 -0700

    seeder

---
+++ heroku_seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (first_name, last_name, email, hashed_password, gender, gender_pref, img_url, spectrum, likes_puns, favorite_pet, spontaneous, into_tech, introvert, liked_users, blocked_users)
+VALUES
+    ('demo', 'user', 'demouser@demouser.com', 'pbkdf2:sha256:150000$Kae4NpQC$0f5d41ceda81c71f16e5c4f7c5651b12e3bb9aa631ff9c2c9a979a6781b7fe7f', 'nonbinary', 'all', null, 'asexual', true, 'dogs', true, true, true [], []);
