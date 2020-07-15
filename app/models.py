f945e0221d5bb4e1173da5511ec740a78f6f3fb3
aderyn1121 <zaristus@gmail.com>
2020-07-15 09:53:15 -0700

    routes working

--- app/models.py
+++ app/models.py
@@ -3,8 +3,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-def _get_date():
-    return datetime.datetime.now()
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -14,17 +12,18 @@ class User(db.Model):
     last_name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     hashed_password = db.Column(db.String(128), nullable=False)
-    gender_pref = db.Column(db.String(5))
+    gender_pref = db.Column(db.String(20))
+    img_url = db.Column(db.String)
     spectrum = db.Column(db.String(20), nullable=False)
     likes_puns = db.Column(db.Boolean, default=True)
-    favorite_pet = db.Column(db.String(10))
+    favorite_pet = db.Column(db.String(20))
     spontaneous = db.Column(db.Boolean, default=True)
     into_tech = db.Column(db.Boolean, default=True)
     introvert = db.Column(db.Boolean, default=True)
     liked_users = db.Column(db.ARRAY(db.Integer))
-    blocked_users = db.Column(db.ARRAY(db.Integer))
+    blocked_users = db.Column(db.ARRAY(db.Integer), default=[])
 
-     @property
+    @property
     def password(self):
         return self.hashed_password
 
@@ -36,16 +35,17 @@ class User(db.Model):
         return check_password_hash(self.hashed_password, password)
 
     def to_dict(self):
-        return { "id": self.id,
+        return {"id": self.id,
                 "firstName": self.first_name,
                 "lastName": self.last_name,
                 "gender": self.gender,
                 "genderPref": self.gender_pref,
-                "spectrum": self.spectrum, 
+                "imgUrl": self.img_url,
+                "spectrum": self.spectrum,
                 "likesPuns": self.likes_puns,
                 "favPet": self.favorite_pet,
                 "spontaneous": self.spontaneous,
-                "into_tech": self.into_tech,
+                "intoTech": self.into_tech,
                 "introvert": self.introvert,
                 "liked": self.liked_users,
                 "blocked": self.blocked_users
@@ -55,9 +55,17 @@ class User(db.Model):
 class Message(db.Model):
     __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.Date, default=_get_date)
     sender_id = db.Column(db.Integer, db.ForeignKey('users.id'),
                           nullable=False)
-    recipient = db.Column(db.Integer, db.ForeignKey('users.id'),
-                          nullable=False)
+    recipient_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                             nullable=False)
     content = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "createdAt": self.created_at,
+            "sender": self.sender_id,
+            "recipient": self.recipient_id,
+            "content": self.content
+        }
--- app/routes/api.py
+++ app/routes/api.py
@@ -1,12 +1,95 @@
 from flask import Blueprint, request, jsonify
 import jwt
 
-from ..models import db
+from ..models import db, User, Message
 from ..config import Configuration
 
-bp = Blueprint("api", __name__, url_prefix="/api")
+api = Blueprint("api", __name__, url_prefix="/api")
 
 
-@bp.route('/')
-def simple():
-    return render_template('main_page.html')
+@api.route('/users', methods=["POST"])
+def signup():
+    print(request.json)
+    data = request.json
+    user = User(
+                first_name=data["firstName"],
+                last_name=data["lastName"],
+                email=data['email'],
+                password=data['password'],
+                gender=data['gender'],
+                gender_pref=data['genderPref'],
+                img_url=data['imgUrl'],
+                spectrum=data['spectrum'],
+                likes_puns=data['likesPuns'],
+                favorite_pet=data['favPet'],
+                spontaneous=data['spontaneous'],
+                into_tech=data['intoTech'],
+                introvert=data['introvert'],
+                liked_users=[],
+                blocked_users=[]
+                )
+    db.session.add(user)
+    db.session.commit()
+    
+    access_token = jwt.encode({'email': user.email}, Configuration.SECRET_KEY)
+    return {'access_token': access_token.decode('UTF-8'),
+            'user': user.to_dict()}
+
+
+@api.route('/users/session', methods=["POST"])
+def login():
+    data = request.json
+    user = User.query.filter(User.email == data['email']).first()
+    if not user:
+        return {"error": "Email not found"}, 422
+    if user.check_password(data['password']):
+        access_token = jwt.encode({'email': user.email},
+                                  Configuration.SECRET_KEY)
+        return {'access_token': access_token.decode('UTF-8'),
+                'user': user.to_dict()}
+    else:
+        return {"error": "Incorrect password"}, 401
+
+
+@api.route('/messages/<int:userId>', methods=["GET"])
+def get_messages(userId):
+    fetched_messages = Message.query.filter(
+        (Message.sender_id == userId) | (Message.recipient_id == userId)).all()
+    messages = [message.to_dict for message in fetched_messages]
+    return jsonify({"messages": messages})
+
+
+@api.route('/messages/<int:userId>', methods=["POST"])
+def post_message():
+    data = request.json
+    try:
+        message = Message(
+            sender_id=data['sender'],
+            recipient_id=data['recipient'],
+            content=data['content']
+        )
+        db.session.add(message)
+        db.session.commit()
+        return jsonify({"message": message.to_dict})
+    except AssertionError as err_message:
+        print(str(err_message))
+        return jsonify({"error": str(err_message)}), 400
+
+
+@api.route('/like/<int:likedUserId>', methods=["POST"])
+def like_user(likedUserId):
+    data = request.json
+    liker_id = data['likerId']
+    user = User.query.filter_by(id=liker_id).first()
+    user.liked_users = user.liked_users.append(likedUserId)
+    db.session.commit()
+
+
+@api.route('/unlike/<int:unlikedId>', methods=["POST"])
+def unlike(unlikedId):
+    data = request.json
+    unlikerId = data['unlikerId']
+    user = User.query.filter_by(id=unlikerId)
+    user.liked_users.remove(unlikedId)
+
+
